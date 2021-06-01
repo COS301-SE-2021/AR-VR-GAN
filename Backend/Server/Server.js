66623ecb75addb5f606cfc6d0f22c906d9418ad5
ecoryndonbakeruni <u19200065@tuks.co.za>
2021-06-01 16:06:48 +0200

    Update Server.js

--- Backend/Server/Server.js
+++ Backend/Server/Server.js
@@ -1,28 +1,38 @@
-const app = require('express')();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+// const app = require('express')();
+// const http = require('http').Server(app);
+// const io = require('socket.io')(http);
 
 
-app.get('/', function(req, res) {
-   res.send("helooooo Ethannnnn");
-});
+// app.get('/', function(req, res) {
+//    res.send("helooooo Ethannnnn");
+// });
 
 
-//Whenever someone connects this gets executed
-io.on('connection', function(socket) {
-   console.log('A user connected');
+// //Whenever someone connects this gets executed
+// io.on('connection', function(socket) {
+//    console.log('A user connected');
 
-   socket.on('Coords',(data)=>
-   {
-      console.log(data);
-   });
+//    socket.on('Coords',(data)=>
+//    {
+//       console.log(data);
+//    });
 
-   //Whenever someone disconnects this piece of code executed
-   socket.on('disconnect', function () {
-      console.log('A user disconnected');
-   });
-});
+//    //Whenever someone disconnects this piece of code executed
+//    socket.on('disconnect', function () {
+//       console.log('A user disconnected');
+//    });
+// });
 
-http.listen(3000, function() {
-   console.log('listening on *:3000');
-});
+// http.listen(3000, function() {
+//    console.log('listening on *:3000');
+// });
+
+const io= require("socket.io")(3000,{
+   cors: {
+      origin: ["http://localhost:8000"],
+   },
+})
+
+io.on("connection", socket => {
+   console.log("this sucks")
+})
