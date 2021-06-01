f859ab291f3e406a583355c694a420488babb04f
Matthew Wood <mattywoodx@gmail.com>
2021-06-01 16:32:10 +0200

    Setting up websocket

--- Backend/Server/Server.js
+++ Backend/Server/Server.js
@@ -1,38 +1,46 @@
-const app = require('express')();
-const http = require('http').Server(app);
-// const io = require('socket.io')(http);
+const http = require('http');
+const WebSocketServer = require('websocket').server;
+const app = require('express');
 
+var server = http.createServer(function(request, response) {
+    console.log((new Date()) + ' Received request for ' + request.url);
+    response.writeHead(404);
+    response.end();
+});
 
-// app.get('/', function(req, res) {
-//    res.send("helooooo Ethannnnn");
-// });
+server.listen(3000, function() {
+    console.log("Server listening on port 3000");
+});
 
+wsServer = new WebSocketServer({
+    httpServer: server,
+    autoAcceptConnections: true
+});
 
-// //Whenever someone connects this gets executed
-// io.on('connection', function(socket) {
-//    console.log('A user connected');
+function originIsAllowed(origin) {
+  return true;
+}
 
-//    socket.on('Coords',(data)=>
-//    {
-//       console.log(data);
-//    });
-
-//    //Whenever someone disconnects this piece of code executed
-//    socket.on('disconnect', function () {
-//       console.log('A user disconnected');
-//    });
-// });
-
-// http.listen(3000, function() {
-//    console.log('listening on *:3000');
-// });
-
-const io= require("socket.io")(3000,{
-   cors: {
-      origin: ["http://localhost:8000"],
-   },
-})
-
-io.on("connection", socket => {
-   console.log("this sucks")
-})
+wsServer.on('request', function(request) {
+    if (!originIsAllowed(request.origin)) {
+      request.reject();
+      console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
+      return;
+    }
+    
+    var connection = request.accept('echo-protocol', request.origin);
+    console.log((new Date()) + ' Connection accepted.');
+    connection.on('message', function(message) {
+        if (message.type === 'utf8') {
+            console.log('Received Message: ' + message.utf8Data);
+            connection.sendUTF(message.utf8Data);
+        }
+        else if (message.type === 'binary') {
+            console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
+            connection.sendBytes(message.binaryData);
+        }
+    });
+    connection.on('close', function(reasonCode, description) {
+        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+    });
+});
