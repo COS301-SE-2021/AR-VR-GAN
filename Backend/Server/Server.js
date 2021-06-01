a7e4dfa4ac6287c75af8026eab668d71b31e1bfd
Matthew Wood <mattywoodx@gmail.com>
2021-06-01 15:24:00 +0200

    Redo Server Side

---
+++ Backend/Server/Server.js
@@ -0,0 +1,23 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+
+app.get('/', function(req, res) {
+   res.send("helooooo Ethannnnn");
+});
+
+
+//Whenever someone connects this gets executed
+io.on('connection', function(socket) {
+   console.log('A user connected');
+
+   //Whenever someone disconnects this piece of code executed
+   socket.on('disconnect', function () {
+      console.log('A user disconnected');
+   });
+});
+
+http.listen(3000, function() {
+   console.log('listening on *:3000');
+});
