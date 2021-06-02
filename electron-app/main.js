b1a0fe567d11d864ed313130f01a3e5f4895584a
Jason Wille <jman89412@gmail.com>
2021-06-02 10:55:33 +0200

    Create main.js

---
+++ electron-app/main.js
@@ -0,0 +1,22 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600
+    })
+
+    win.loadFile('content/index.html')
+}
+
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', function () {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') app.quit()
+})
