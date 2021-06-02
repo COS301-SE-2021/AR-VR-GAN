b22af76679aa3b9a979c295987b0a33bddc3360d
Matthew Wood <mattywoodx@gmail.com>
2021-06-02 21:31:44 +0200

    Removing Irrelevant Test

--- ar-vr-gan/src/app/app.component.spec.ts
+++ ar-vr-gan/src/app/app.component.spec.ts
@@ -25,11 +25,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('ar-vr-gan');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ar-vr-gan app is running!');
-  });
 });
