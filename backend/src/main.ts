ff70f4a51ba4fe3fc9684982049fbee102c592b8
Jason Wille <jman89412@gmail.com>
2021-06-19 12:25:25 +0200

    Attempting gRPC

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,42 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ModelModule } from './model/model.module';
+import { Transport } from '@nestjs/microservices';
+import { join } from 'path';
+import { Logger } from '@nestjs/common';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   // await app.listen(3000);
+
+//   const configService = app.get(ConfigService);
+  
+//   await app.connectMicroservice<MicroserviceOptions>({
+//     transport: Transport.GRPC,
+//     options: {
+//       package: 'model',
+//       protoPath: join(process.cwd(), 'src/model/model.proto'),
+//       url: configService.get('localhost:5000')
+//     },
+//   });
+ 
+//   app.startAllMicroservices();
+// }
+// bootstrap();
+
+const logger = new Logger('Main');
+const microserviceOptions = {
+  transport: Transport.GRPC,  
+  options: {
+    package: 'model', 
+    protoPath: join(__dirname, '../src/model/model.proto'), 
+  },
+};
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice(ModelModule, microserviceOptions);
+  app.listen(() => {
+    logger.log('Microservice is listening...');
+  });
 }
 bootstrap();
--- backend/src/model/model.controller.ts
+++ backend/src/model/model.controller.ts
@@ -1,4 +1,18 @@
 import { Controller } from '@nestjs/common';
+import { GrpcMethod } from '@nestjs/microservices';
+
+interface Request {
+    data: number[];
+}
+
+interface Response {
+    sum: number;
+}
 
 @Controller('model')
-export class ModelController {}
+export class ModelController {
+    @GrpcMethod('ModelController', 'HandleCoords')
+    handleCoords(request: Request, metadata: any): Response {
+        return {sum: 3};
+    }
+}
