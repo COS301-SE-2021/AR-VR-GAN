29b4e7c6e8b1ae2183edb56f4820843ea6fe22b9
Jason Wille <jman89412@gmail.com>
2021-06-19 12:53:02 +0200

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,30 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ModelModule } from './model/model.module';
 import { Transport } from '@nestjs/microservices';
 import { join } from 'path';
-import { Logger } from '@nestjs/common';
 
-// async function bootstrap() {
-//   const app = await NestFactory.create(AppModule);
-//   // await app.listen(3000);
-
-//   const configService = app.get(ConfigService);
-  
-//   await app.connectMicroservice<MicroserviceOptions>({
-//     transport: Transport.GRPC,
-//     options: {
-//       package: 'model',
-//       protoPath: join(process.cwd(), 'src/model/model.proto'),
-//       url: configService.get('localhost:5000')
-//     },
-//   });
- 
-//   app.startAllMicroservices();
-// }
-// bootstrap();
-
-const logger = new Logger('Main');
 const microserviceOptions = {
   transport: Transport.GRPC,  
   options: {
@@ -34,9 +12,11 @@ const microserviceOptions = {
 };
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice(ModelModule, microserviceOptions);
-  app.listen(() => {
-    logger.log('Microservice is listening...');
-  });
+  const app = await NestFactory.create(AppModule);
+  
+  app.connectMicroservice(microserviceOptions);              
+  
+  await app.startAllMicroservices();                                 
+  await app.listen(3000);
 }
 bootstrap();
