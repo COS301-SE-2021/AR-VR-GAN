8dce1aeeaa022bc54fbbc26d70981b6cb6ee1727
Jason Wille <jman89412@gmail.com>
2021-06-19 18:59:02 +0200

    Update app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,10 +4,12 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './user/user.module';
 import { ModelModule } from './model/model.module';
+import { UploadModule } from './upload/upload.module';
 import config from './config/keys'
 
+
 @Module({
-  imports: [ModelModule, UsersModule, MongooseModule.forRoot(config.mongoURI)],
+  imports: [UploadModule, ModelModule, UsersModule, MongooseModule.forRoot(config.mongoURI)],
   controllers: [AppController],
   providers: [AppService],
 })
