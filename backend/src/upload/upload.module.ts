7c3f2bc4fa99b26c6b05447075db922631d1e1ef
Jason Wille <jman89412@gmail.com>
2021-06-19 19:10:03 +0200

    Update upload.module.ts

--- backend/src/upload/upload.module.ts
+++ backend/src/upload/upload.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
 import { UploadController } from './upload.controller';
 import { UploadService } from './upload.service';
 
