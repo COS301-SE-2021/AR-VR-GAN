5a3f5e52f6e93f4c268edaad1f2c5ac42f5005a8
ecoryndonbakeruni <u19200065@tuks.co.za>
2021-06-19 18:04:36 +0200

    edited model controller

--- backend/src/model/model.controller.ts
+++ backend/src/model/model.controller.ts
@@ -2,18 +2,17 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { GrpcMethod } from '@nestjs/microservices';
 import { Response } from './interfaces/response.interface'
 import { Request } from './interfaces/request.interface'
+import { ModelService } from './model.service';
 
 @Controller('model')
 export class ModelController {
+
+    constructor(private modelService: ModelService) {}
+
+
     @GrpcMethod('ModelController', 'HandleCoords')
     handleCoords(request: Request): Response {
-        let sum = 0;
-
-        for (let i = 0; i < request.data.length; i++) {
-            sum += request.data[i]
-        }
-
-        return { sum };
+        return { sum : this.modelService.handleCoords(request) };
     }
 
     @Post('testGRPC')
