8f6c2e3b70d9825e2d9a4164090f3f2dd84ea1df
ecoryndonbakeruni <u19200065@tuks.co.za>
2021-06-19 18:05:28 +0200

    edited model service

--- backend/src/model/model.service.ts
+++ backend/src/model/model.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { Response } from './interfaces/response.interface'
+import { Request } from './interfaces/request.interface'
 
 @Injectable()
-export class ModelService {}
+export class ModelService {
+    public handleCoords(request: Request): number {
+        let sum = 0;
+
+        for (let i = 0; i < request.data.length; i++) {
+            sum += request.data[i]
+        }
+
+        return  sum;
+    }
+}
