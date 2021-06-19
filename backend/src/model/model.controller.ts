1b6146c91feaac8021849e77ffbfacd3bdf503a4
Jason Wille <jman89412@gmail.com>
2021-06-19 14:13:25 +0200

    Update model.controller.ts

--- backend/src/model/model.controller.ts
+++ backend/src/model/model.controller.ts
@@ -1,10 +1,12 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Logger } from '@nestjs/common';
 import { GrpcMethod } from '@nestjs/microservices';
 import { Response } from './interfaces/response.interface'
 import { Request } from './interfaces/request.interface'
 
 @Controller('model')
 export class ModelController {
+    private logger = new Logger('ModelController');
+
     @GrpcMethod('ModelController', 'HandleCoords')
     handleCoords(request: Request): Response {
         let sum = 0;
@@ -12,6 +14,8 @@ export class ModelController {
         for (let i = 0; i < request.data.length; i++) {
             sum += request.data[i]
         }
+
+        this.logger.log(`The sum of the numbers is: ${sum}`);
 
         return { sum };
     }
