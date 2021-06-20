498132dfe18b5046ea263c02dc8718d1f8ff18c9
Matthew Wood <mattywoodx@gmail.com>
2021-06-20 20:33:33 +0200

    Updates

--- generativeModelFiles/test_model_generator.py
+++ generativeModelFiles/test_model_generator.py
@@ -35,10 +35,6 @@ class TestModelGenerator(unittest.TestCase):
         self.assertTrue(self.generator.generateImage("testing/images/testingImages.jpeg"))
         self.assertTrue(self.generator.generateImage("testing/images/testingImages.jpg"))
 
-        # with self.assertRaises(ModelException) as exceptionOne:
-        #     self.generator.generateImage("RandomImageName.py")
-        # self.assertEqual(exceptionOne.exception.message, "File extension must be either be png, jpg, jpeg")
-
 
 
 if __name__ == "__main__":
