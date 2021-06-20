8279e7816b9cc6d1425fef79981626c144fb3753
ecoryndonbakeruni <u19200065@tuks.co.za>
2021-06-20 15:15:38 +0200

    neatend uo controller

--- backend/src/model/model.service.ts
+++ backend/src/model/model.service.ts
@@ -1,9 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { Response } from './interfaces/response.interface';
 import { Request } from './interfaces/request.interface';
-import { PythonShell } from 'python-shell';
 import { join } from 'path';
-//import { spawn } from 'child_process';
 
 @Injectable()
 export class ModelService {
@@ -22,29 +19,15 @@ export class ModelService {
 
         
         var myPythonScriptPath = join(__dirname, '../../src/model/mocks/py-script.py');
-        let options = {
-            pythonOptions: ['-u'], // get print results in real-time
-            scriptPath: join(__dirname, '../../src/model/mocks'),
-            args: [request.data.toString()]
-          };
-
-        
-        // PythonShell.run('py-script.py', options, function (err, results) {
-        //     if (err) throw err;
-            
-        //     console.log(results[0]);
-        //     num = results[0];
-        // });
 
         const spawn = require("child_process").spawn;
-          var process = spawn('python',[myPythonScriptPath,request.data.toString()]);
-          process.stdout.on('data',async data =>{
-              this.num = data.toString().trim()
-              console.log(data.toString().trim());
-              return this.num;
-          })
 
-        
+        var process = spawn('python',[myPythonScriptPath,request.data.toString()]);
+          
+        process.stdout.on('data',async data =>{
+              this.num = data.toString().trim()
+              return this.num;
+        })
 
         return this.num;
     }
