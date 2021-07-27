2dde0623bc1bb8253026423b97f419ec5bf9815d
Jason Wille <jman89412@gmail.com>
2021-07-27 12:21:29 +0200

    Adding an isAdmin field to the user table.

--- backend/src/user/schemas/user.schema.ts
+++ backend/src/user/schemas/user.schema.ts
@@ -4,4 +4,8 @@ export const UserSchema = new mongoose.Schema({
     username: String,
     email: String,
     password: String,
+    isAdmin: {
+        type: Boolean,
+        default: false
+    }
 });
