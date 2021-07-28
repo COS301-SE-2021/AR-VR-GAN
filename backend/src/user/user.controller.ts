1927b502238dff1f21a2717e937cd3dbcd4fe1d2
Jason Wille <jman89412@gmail.com>
2021-07-28 13:00:42 +0200

    Change all to post requests.

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,41 +1,45 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Param, Post } from '@nestjs/common';
 import { RegisterUserDto } from './dto/register-user.dto';
 import { UserService } from './user.service';
 import { User } from './interfaces/user.interface';
 import { UserResponse } from './dto/user-response.dto';
 import { LoginUserDto } from './dto/login-user.dto';
+import { GetAllUsersDto, GetAllUsersResponse } from './dto/get-all-users.dto';
+import { GetUserByUsernameDto } from './dto/get-user-by-username.dto';
+import { GetUserByUsernameResponse } from './dto/get-user-by-username.dto';
+import { UpdateUserByUsernameDto } from './dto/update-user-by-username.dto';
 
 @Controller('user')
 export class UserController {
     constructor(private readonly userService: UserService) {}
+
+    @Post('/register')
+    registerUser(@Body() registerUserDto: RegisterUserDto): Promise<UserResponse> {
+        return this.userService.registerUser(registerUserDto);
+    }
 
     @Post('/login')
     loginUser(@Body() loginUserDto: LoginUserDto): Promise<UserResponse> {
         return this.userService.loginUser(loginUserDto);
     }
 
-    @Get()
-    getAllUsers(): Promise<User[]> {
-        return this.userService.getAllUsers();
+    @Post('/getAllUsers')
+    getAllUsers(@Body() getAllUsersDto: GetAllUsersDto): Promise<GetAllUsersResponse> {
+        return this.userService.getAllUsers(getAllUsersDto.jwtToken);
     }
 
-    @Get(':id')
-    getUserById(@Param('id') id): Promise<User> {
-        return this.userService.getUserById(id);
+    @Post('/getUserByUsername')
+    getUserByUsername(@Body() getUserByUsernameDto : GetUserByUsernameDto): Promise<GetUserByUsernameResponse> {
+        return this.userService.getUserByUsername(getUserByUsernameDto.jwtToken, getUserByUsernameDto.username);
     }
 
-    @Post()
-    registerUser(@Body() registerUserDto: RegisterUserDto): Promise<UserResponse> {
-        return this.userService.registerUser(registerUserDto);
+    @Post('/deleteUserByUsername')
+    deleteUserByUsername(@Body() deleteUserByUsername: GetUserByUsernameDto): Promise<UserResponse> {
+        return this.userService.deleteUserByUsername(deleteUserByUsername.jwtToken, deleteUserByUsername.username);
     }
 
-    @Delete(':id')
-    deleteUserById(@Param('id') id): Promise<User> {
-        return this.userService.deleteUserById(id);
-    }
-
-    @Put(':id')
-    updateUserWithId(@Param('id') id, @Body() updateUserDto: RegisterUserDto): Promise<User> {
-        return this.userService.updateUserWithId(id, updateUserDto);
+    @Post('/updateUserWithUsername')
+    updateUserWithUsername(@Body() updateUserByUsername: UpdateUserByUsernameDto): Promise<UserResponse> {
+        return this.userService.updateUserWithUsername(updateUserByUsername);
     }
 }
