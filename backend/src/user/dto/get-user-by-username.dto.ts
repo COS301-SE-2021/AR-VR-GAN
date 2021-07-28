42a64b6b46234e8b9c4f524125b28914500bddd3
Jason Wille <jman89412@gmail.com>
2021-07-28 13:01:02 +0200

    Create get-user-by-username.dto.ts

---
+++ backend/src/user/dto/get-user-by-username.dto.ts
@@ -0,0 +1,18 @@
+import { User } from "../interfaces/user.interface";
+
+export class GetUserByUsernameResponse {
+    readonly success: boolean;
+    readonly message: string;
+    readonly user: User;
+
+    constructor(success: boolean, message: string, user: User) {
+        this.success = success;
+        this.message = message;
+        this.user = user;
+    }
+}
+
+export class GetUserByUsernameDto {
+    readonly jwtToken: string;
+    readonly username: string;
+}
