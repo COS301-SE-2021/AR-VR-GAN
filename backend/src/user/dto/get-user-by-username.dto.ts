d7ce3ad79dfa7f47310b4bd3d899d18164982b57
ecoryndonbakeruni <u19200065@tuks.co.za>
2021-07-30 09:14:50 +0200

    updated username dto

--- backend/src/user/dto/get-user-by-username.dto.ts
+++ backend/src/user/dto/get-user-by-username.dto.ts
@@ -15,4 +15,10 @@ export class GetUserByUsernameResponse {
 export class GetUserByUsernameDto {
     readonly jwtToken: string;
     readonly username: string;
+
+    constructor(jwtToken: string, username: string) {
+        this.jwtToken = jwtToken;
+        this.username = username;
+
+    }
 }
