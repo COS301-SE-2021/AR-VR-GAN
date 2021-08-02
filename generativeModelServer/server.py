d6a945c5242d56c6cb22f16426bed616c6eed2d7
kabelo-tuks <u19247070@tuks.co.za>
2021-08-02 15:24:41 +0200

    Created server file

---
+++ generativeModelServer/server.py
@@ -0,0 +1,35 @@
+from generativeModelServer.modelGenerator_pb2 import LoadModelResponse
+import grpc
+import concurrent
+from concurrent import futures
+
+import modelGenerator_pb2
+import modelGenerator_pb2_grpc
+from generativeModelFiles.modelGenerator import ModelGenerator
+
+class ModelGenerationServicer(modelGenerator_pb2_grpc.ModelGenerationServicer):
+    m_generator = ModelGenerator()
+    def GenerateImage(self, request, context):
+        pass
+
+    def LoadDataset(self, request, context):
+        pass
+
+    def TrainModel(self, request, context):
+        pass
+
+    def LoadModel(self, request, context):
+        print(f"The Model: {request.modelName}")
+        
+        response = modelGenerator_pb2.LoadModelResponse()
+        response.successful = True
+        response.message = "Successful"
+        return response
+
+if __name__ == "__main__":
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=20))
+    modelGenerator_pb2_grpc.add_ModelGenerationServicer_to_server(ModelGenerationServicer(), server)
+    port_number = server.add_insecure_port('[::]:50051') # Change this to secure_port when we are not in development
+    print(f"Model Generator Server Started. Listening on port {port_number}")
+    server.start()
+    server.wait_for_termination()
