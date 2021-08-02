73adb433c0e3b2710a858196f13ba7d4e558b4d1
Jason Wille <jman89412@gmail.com>
2021-08-02 16:03:53 +0200

    Revert "Merge remote-tracking branch 'origin/feature-backend-streaming-ethan' into feature-backend-user-jason"
    This reverts commit a7d851753f1bf06f37e77373941e58e64958397b, reversing
    changes made to dbddf8ae03439a5bd206af20c8b5c7604b325d41.

--- backend/src/model/interfaces/response.interface.ts
+++ backend/src/model/interfaces/response.interface.ts
@@ -1,3 +1,3 @@
 export interface Response {
-    data: any;
+    data: number;
 }
--- backend/src/model/model.controller.ts
+++ backend/src/model/model.controller.ts
@@ -1,13 +1,10 @@
-import { Body, Controller, Post, Logger } from '@nestjs/common';
-import { GrpcMethod,GrpcStreamMethod } from '@nestjs/microservices';
+import { Controller } from '@nestjs/common';
+import { GrpcMethod, GrpcStreamMethod } from '@nestjs/microservices';
 import { Response } from './interfaces/response.interface'
 import { ResponsePython } from './interfaces/responsePython.interface';
 import { Request } from './interfaces/request.interface'
 import { ModelService } from './model.service';
-import { Observable, ReplaySubject, Subject } from 'rxjs';
-import { createReadStream } from 'fs';
-import { join } from 'path';
-import * as fs from 'fs';
+import { Observable, Subject } from 'rxjs';
 
 @Controller('model')
 export class ModelController {
@@ -16,37 +13,14 @@ export class ModelController {
     @GrpcStreamMethod()
     handleCoords(messages: Observable<Request>): Observable<Response> {
         const subject = new Subject<Response>();
-    //     const onNext = (message: Request) => {
-    //         subject.next({
-    //             data: this.modelService.handleCoords(message)
-    //         });
-    //     };
 
-    //     const onComplete = () => subject.complete();
-
-    //     messages.subscribe({
-    //         next: onNext,
-    //         complete: onComplete,
-    //     });
-
-    //     return subject.asObservable();
-    // }
-
-    /**
-     * streaming the image back to the client
-     * @param messages 
-     * @returns 
-     */
-    @GrpcStreamMethod()
-    handleCoords(messages: Observable<Request>): Observable<Response> {
-        const subject = new Subject<Response>();
-        const img = fs.readFileSync(join(__dirname, '../../uploads/capstone.jpg'));
         const onNext = (message: Request) => {
             subject.next({
-                data: img 
+                data: this.modelService.handleCoords(message)
             });
         };
 
+        const onComplete = () => subject.complete();
 
         messages.subscribe({
             next: onNext,
