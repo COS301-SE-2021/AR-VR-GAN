0567b612f142aeae0e18352ef92beb5943fc3161
ecoryndonbakeruni <u19200065@tuks.co.za>
2021-08-02 11:31:38 +0200

    streaming function added

--- backend/src/model/model.controller.ts
+++ backend/src/model/model.controller.ts
@@ -1,17 +1,39 @@
 import { Body, Controller, Post, Logger } from '@nestjs/common';
-import { GrpcMethod } from '@nestjs/microservices';
+import { GrpcMethod,GrpcStreamMethod } from '@nestjs/microservices';
 import { Response } from './interfaces/response.interface'
 import { ResponsePython } from './interfaces/responsePython.interface';
 import { Request } from './interfaces/request.interface'
 import { ModelService } from './model.service';
+import { Observable, ReplaySubject, Subject } from 'rxjs';
 
 @Controller('model')
 export class ModelController {
     constructor(private modelService: ModelService) {}
 
-    @GrpcMethod('ModelController', 'HandleCoords')
-    handleCoords(request: Request): Response {
-        return { sum : this.modelService.handleCoords(request) };
+    // @GrpcMethod('ModelController', 'HandleCoords')
+    // handleCoords(request: Request): Response {
+    //     return { sum : this.modelService.handleCoords(request) };
+    // }
+
+    // @GrpcStreamMethod('ModelController', 'HandleCoords')
+    // handleCoords(request: Observable<Request>): Observable<Response> {
+    //     return ({ sum : this.modelService.handleCoords(Observable<request>) }).asObservable();
+    // }
+    @GrpcStreamMethod('ModelController', 'HandleCoords')
+    handleCoords(data$: Observable<Request>): Observable<Response> {
+      const hero$ = new Subject<Response>();
+  
+      const onNext = (request: Request) => {
+        const item = { sum : this.modelService.handleCoords(request) };
+        hero$.next(item);
+      };
+      const onComplete = () => hero$.complete();
+      data$.subscribe({
+        next: onNext,
+        complete: onComplete,
+      });
+
+      return hero$.asObservable();
     }
 
     @GrpcMethod('ModelController', 'RunPython')
@@ -19,10 +41,10 @@ export class ModelController {
         return { data : this.modelService.runPython(request) };
     }
 
-    @Post('testGRPC')
-    testGRPC(@Body() request: Request): Response {
-        return this.handleCoords(request);
-    }
+    // @Post('testGRPC')
+    // testGRPC(@Body() request: Request): Response {
+    //     return this.handleCoords(request);
+    // }
 
     @Post('testPython')
     testPython(@Body() request: Request): ResponsePython {
