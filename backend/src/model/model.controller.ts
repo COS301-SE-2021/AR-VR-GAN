51ed5927c559cb34764e57dfa5427c48415ef795
Jason Wille <jman89412@gmail.com>
2021-08-02 11:53:09 +0200

    Create logic in the controller for the gRPC stream function.

--- backend/src/model/model.controller.ts
+++ backend/src/model/model.controller.ts
@@ -1,31 +1,37 @@
-import { Body, Controller, Post, Logger } from '@nestjs/common';
-import { GrpcMethod } from '@nestjs/microservices';
+import { Controller } from '@nestjs/common';
+import { GrpcMethod, GrpcStreamMethod } from '@nestjs/microservices';
 import { Response } from './interfaces/response.interface'
 import { ResponsePython } from './interfaces/responsePython.interface';
 import { Request } from './interfaces/request.interface'
 import { ModelService } from './model.service';
+import { Observable, Subject } from 'rxjs';
 
 @Controller('model')
 export class ModelController {
     constructor(private modelService: ModelService) {}
 
-    @GrpcMethod('ModelController', 'HandleCoords')
-    handleCoords(request: Request): Response {
-        return { sum : this.modelService.handleCoords(request) };
+    @GrpcStreamMethod()
+    handleCoords(messages: Observable<Request>): Observable<Response> {
+        const subject = new Subject<Response>();
+
+        const onNext = (message: Request) => {
+            subject.next({
+                data: this.modelService.handleCoords(message)
+            });
+        };
+
+        const onComplete = () => subject.complete();
+
+        messages.subscribe({
+            next: onNext,
+            complete: onComplete,
+        });
+
+        return subject.asObservable();
     }
 
     @GrpcMethod('ModelController', 'RunPython')
     runPython(request: Request): ResponsePython {
         return { data : this.modelService.runPython(request) };
-    }
-
-    @Post('testGRPC')
-    testGRPC(@Body() request: Request): Response {
-        return this.handleCoords(request);
-    }
-
-    @Post('testPython')
-    testPython(@Body() request: Request): ResponsePython {
-        return  this.runPython(request);
     }
 }
