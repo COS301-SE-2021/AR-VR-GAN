b8b8fb2c2ee29f0b3813f637ee45af57423c745c
ecoryndonbakeruni <u19200065@tuks.co.za>
2021-08-02 15:39:48 +0200

    added image stream function

--- backend/src/model/model.controller.ts
+++ backend/src/model/model.controller.ts
@@ -5,18 +5,46 @@ import { ResponsePython } from './interfaces/responsePython.interface';
 import { Request } from './interfaces/request.interface'
 import { ModelService } from './model.service';
 import { Observable, ReplaySubject, Subject } from 'rxjs';
+import { createReadStream } from 'fs';
+import { join } from 'path';
+import * as fs from 'fs';
 
 @Controller('model')
 export class ModelController {
     constructor(private modelService: ModelService) {}
 
+    // @GrpcStreamMethod()
+    // handleCoords(messages: Observable<Request>): Observable<Response> {
+    //     const subject = new Subject<Response>();
+
+    //     const onNext = (message: Request) => {
+    //         subject.next({
+    //             data: this.modelService.handleCoords(message)
+    //         });
+    //     };
+
+    //     const onComplete = () => subject.complete();
+
+    //     messages.subscribe({
+    //         next: onNext,
+    //         complete: onComplete,
+    //     });
+
+    //     return subject.asObservable();
+    // }
+
+    /**
+     * streaming the image back to the client
+     * @param messages 
+     * @returns 
+     */
     @GrpcStreamMethod()
     handleCoords(messages: Observable<Request>): Observable<Response> {
         const subject = new Subject<Response>();
-
+        const img = fs.readFileSync(join(__dirname, '../../uploads/capstone.jpg'));
         const onNext = (message: Request) => {
             subject.next({
-                data: this.modelService.handleCoords(message)
+                data: img 
             });
         };
 
