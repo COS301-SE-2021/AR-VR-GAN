a547307e663e9e42fd2585f1eaac349c239bed14
ZachariahIdiculla <u17067210@tuks.co.za>
2021-08-03 12:38:30 +0200

    Removed user folder, not needed currently for testing

--- backend/src/user/dto/user-response.dto.ts
+++
@@ -1,9 +0,0 @@
-export class UserResponse {
-    readonly success: boolean;
-    readonly message: string;
-
-    constructor(success: boolean, message: string) {
-        this.success = success;
-        this.message = message;
-    }
-}
--- backend/src/user/interfaces/user.interface.ts
+++
@@ -1,9 +0,0 @@
-export interface User {
-    id?: string;
-    username: string;
-    email: string;
-    password: string;
-    isAdmin: boolean;
-    jwtToken: string;
-    lastLoggedIn: number;
-}
