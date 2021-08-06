bf1d8e13091ee75e2567378c5d7993380afd38b6
Matthew Wood <mattywoodx@gmail.com>
2021-08-06 11:06:24 +0200

    Update MovementOfPicture.cs

--- Unity/ARVRGAN/Assets/Scripts/Movement/MovementOfPicture.cs
+++ Unity/ARVRGAN/Assets/Scripts/Movement/MovementOfPicture.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class MovementOfPicture : MonoBehaviour
 {
+    /**
+     * Target is camera/player coordinates
+     * Picture moves with player
+     * LookAt points towards player to ensure pic stays pointing at user during rotation
+     * Offset to ensure picture stays at distance away from player/camera
+     */
     public Transform target;
     public Vector3 offset;
+    public Vector3 rotationOffset;
     public float smooth = 0.125f;
     // Update is called once per frame
     void FixedUpdate()
@@ -13,7 +20,11 @@ public class MovementOfPicture : MonoBehaviour
         Vector3 pos = target.position + offset;
         Vector3 smoothPos = Vector3.Lerp(transform.position, pos, smooth);
         transform.position = smoothPos;
-        
-        transform.LookAt(target);
+
+        Vector3 look = pos + rotationOffset;
+
+        //transform.rotation = 
+
+        //transform.LookAt(look);
     }
 }
