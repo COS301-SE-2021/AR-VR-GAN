61e104c38e2e27e80e19de8fd5e0433336345781
Matthew Wood <mattywoodx@gmail.com>
2021-08-10 11:04:13 +0200

    Update GRPCService.cs

--- Unity/ARVRGAN/Assets/Scripts/GRPC/GRPCService.cs
+++ Unity/ARVRGAN/Assets/Scripts/GRPC/GRPCService.cs
@@ -13,20 +13,27 @@ public class GRPCService : MonoBehaviour
     //public Material imgPlane;
     public GameObject plane;
     public GameObject camera;
+    public Vector3 pastCoords;
 
     private void Start()
     {
-        //plane.GetComponent<Renderer>().material.color = Color.white;
-        //pythonClient = new GenerativeModel();
-        //pythonClient.FetchImagePython();
+        plane.GetComponent<Renderer>().material.color = Color.white;
+        pythonClient = new GenerativeModel();
+
     }
 
     // Start is called before the first frame update
     void LateUpdate()
     {
-        plane.GetComponent<Renderer>().material.color = Color.cyan;
-        client = new GRPCClient();
-        client.HandleCoords(plane, camera);
+        if (camera.transform.position != pastCoords)
+        {
+            pythonClient.FetchImagePython(plane, camera);
+        }
+
+        pastCoords = camera.transform.position;
+        //plane.GetComponent<Renderer>().material.color = Color.cyan;
+        //client = new GRPCClient();
+        //client.HandleCoords(plane, camera);
         //GenerativeModel gm = new GenerativeModel();
         //gm.FetchImagePython();
     } }
