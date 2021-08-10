06dafb37b0d50badd8a723890af366dce63cf3c2
Jason Wille <jman89412@gmail.com>
2021-08-10 14:15:42 +0200

    Update app.module.ts

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,25 +2,35 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UploadsComponent } from './uploads/uploads.component';
 import { HeaderComponent } from './header/header.component';
 import { CoordsComponent } from './coords/coords.component';
+import { LoginComponent } from './login/login.component';
+import { LandingComponent } from './landing/landing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UploadsComponent,
     HeaderComponent,
-    CoordsComponent
+    CoordsComponent,
+    LoginComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MatGridListModule,
-    MatToolbarModule
+    MatToolbarModule,
+    RouterModule.forRoot([
+      {path: 'login', component: LoginComponent},
+      {path: 'landing', component: LandingComponent},
+      {path: '', redirectTo: '/login', pathMatch: 'full'},
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
