a849d9418cd1afc239eb0226e1ee0006c6a8979d
Jason Wille <jman89412@gmail.com>
2021-08-11 14:35:02 +0200

    Login Component

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,4 +1,3 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,18 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  constructor(private http: HttpClient) { }
+  constructor() { }
 
   ngOnInit(): void {
-  }
-
-  loginUser(username: string, password: string): void {
-    this.http.post<any>('http://localhost:3000/user/login/', {'username': username, 'password': password}).subscribe(resp => {
-      if (resp.success) {
-        console.log('Success');
-      } else {
-        console.log('Fail');
-      }
-    });
   }
 }
