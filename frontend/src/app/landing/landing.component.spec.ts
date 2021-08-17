926b52cc63e1930c9f0150a005451b664cd425bf
Jason Wille <jman89412@gmail.com>
2021-08-17 12:54:34 +0200

    Update landing.component.spec.ts

--- frontend/src/app/landing/landing.component.spec.ts
+++ frontend/src/app/landing/landing.component.spec.ts
@@ -1,5 +1,10 @@
 import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { CoordsComponent } from '../coords/coords.component';
+import { HeaderComponent } from '../header/header.component';
+import { UploadsComponent } from '../uploads/uploads.component';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 import { LandingComponent } from './landing.component';
 
@@ -9,8 +14,8 @@ describe('LandingComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LandingComponent ],
-      imports: [ HttpClientModule ]
+      declarations: [ LandingComponent, UploadsComponent, HeaderComponent, CoordsComponent ],
+      imports: [ HttpClientModule, RouterTestingModule, MatGridListModule ]
     })
     .compileComponents();
   });
