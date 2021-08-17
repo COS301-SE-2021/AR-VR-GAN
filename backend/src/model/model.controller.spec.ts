161d2121449c00ab86afd46f590ea66883c08c49
ZachariahIdiculla <u17067210@tuks.co.za>
2021-08-17 08:50:54 +0200

    Update model.controller.spec.ts

--- backend/src/model/model.controller.spec.ts
+++ backend/src/model/model.controller.spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { ModelController } from './model.controller';
 import { MockModelService } from './mocks/model.mock';
 import { ModelService } from './model.service';
+import { Observable, Subject } from 'rxjs';
 
 describe('ModelController', () => {
   let controller: ModelController;
@@ -19,15 +20,37 @@ describe('ModelController', () => {
     expect(controller).toBeDefined();
   });
 
-  it('should sum up the data', () => {
-    const dto = { data : [1,2,3] }
-    let sum = 0;
+  // it('should sum up the data', () => {
+  //   const dto = { data : [1,2,3] }
+  //   let sum = 0;
 
-    for (let i = 0; i < dto.data.length; i++) {
-        sum += dto.data[i]
-    }
+  //   for (let i = 0; i < dto.data.length; i++) {
+  //       sum += dto.data[i]
+  //   }
 
-    expect(controller.handleCoords(dto)).toEqual({sum : sum});
+  //   expect(controller.handleCoords(dto)).toEqual({sum : sum});
+  // });
+
+  it('should be proxied', () => {
+    const mockFn = jest.fn(controller.proxy);
+    const subject = new Subject<Response>();
+    // Observable<Request> messages = null;
+     
+            const onNext =async (message: Request) => {
+                var data =await MockModelService.proxy(message)
+                // subject.next({
+                //      data: data.image
+                //  });
+             };
+     
+             const onComplete = () => subject.complete();
+     
+            //  messages.subscribe({
+            //      next: onNext,
+            //      complete: onComplete,
+            //  });
+     
+    expect(controller.proxy).toBe;
   });
 
 });
