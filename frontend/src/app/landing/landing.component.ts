fde537c29c61ba9a021940f4898f5eef712e13b1
Jason Wille <jman89412@gmail.com>
2021-08-17 12:54:38 +0200

    Update landing.component.ts

--- frontend/src/app/landing/landing.component.ts
+++ frontend/src/app/landing/landing.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
 
 @Component({
@@ -9,21 +10,21 @@ import { CookieService } from 'ngx-cookie-service';
 })
 export class LandingComponent implements OnInit {
 
-  constructor(private http: HttpClient, private cookieService: CookieService) { }
-
-  ngOnInit(): void {
+  constructor(private http: HttpClient, private cookieService: CookieService, private router: Router) { 
     if (this.cookieService.check('jwtToken')) {
       this.http.post<any>('http://localhost:3000/user/getUserByJWTToken/', {'jwtToken': this.cookieService.get('jwtToken')}).subscribe(resp => {      
       if (resp.success) {
         if (resp.user.username !== this.cookieService.get('username')) {
-          window.location.replace('http://localhost:4200/login');
+          this.router.navigate(['/login']);
         }
       } else {
-        window.location.replace('http://localhost:4200/login');
+        this.router.navigate(['/login']);
       }
     });
     } else {
-      window.location.replace('http://localhost:4200/login');
+      this.router.navigate(['/login']);
     }
   }
+
+  ngOnInit(): void {}
 }
