d87edffc311b650ccd49e2087d5551b427a09a04
Jason Wille <jman89412@gmail.com>
2021-08-17 12:55:10 +0200

    Update register-form.component.spec.ts

--- frontend/src/app/register-form/register-form.component.spec.ts
+++ frontend/src/app/register-form/register-form.component.spec.ts
@@ -1,5 +1,6 @@
 import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { RegisterFormComponent } from './register-form.component';
 
@@ -10,7 +11,7 @@ describe('RegisterFormComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ RegisterFormComponent ],
-      imports: [ HttpClientModule ]
+      imports: [ HttpClientModule, RouterTestingModule ]
     })
     .compileComponents();
   });
