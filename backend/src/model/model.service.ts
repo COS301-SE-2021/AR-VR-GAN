a89509ac28c2cb9d8326b2b589b9b0cdc7cb214e
ecoryndonbakeruni <u19200065@tuks.co.za>
2021-08-17 20:38:25 +0200

    edited model service

--- backend/src/model/model.service.ts
+++ backend/src/model/model.service.ts
@@ -25,7 +25,6 @@ export class ModelService {
         for (let i = 0; i < request.data.length; i++) {
             sum += request.data[i]
         }
-        console.log(sum)
         return sum;
     }
 
