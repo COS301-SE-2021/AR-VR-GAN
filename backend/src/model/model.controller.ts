a479323749c1dc67d79defdc66206541f2e4bcaf
ecoryndonbakeruni <u19200065@tuks.co.za>
2021-08-17 20:38:08 +0200

    fixed file path

--- backend/src/model/model.controller.ts
+++ backend/src/model/model.controller.ts
@@ -13,7 +13,8 @@ export class ModelController {
         transport: Transport.GRPC,
         options: {
           package: 'model',
-          protoPath: join(__dirname, '../model/model.proto'),
+          protoPath: join(__dirname, '../../src/model/model.proto'),
+          //protoPath: join(__dirname, '../model/model.proto'),
         },
       })
       client: ClientGrpc;
