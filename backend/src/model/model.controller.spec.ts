317098b0346537c5313ef0f4b2c552dd8806917f
ecoryndonbakeruni <u19200065@tuks.co.za>
2021-08-18 09:48:39 +0200

    added model controller testing

--- backend/src/model/model.controller.spec.ts
+++ backend/src/model/model.controller.spec.ts
@@ -2,21 +2,105 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { ModelController } from './model.controller';
 import { MockModelService } from './mocks/model.mock';
 import { ModelService } from './model.service';
+import { INestApplication } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { join } from 'path';
+import * as ProtoLoader from '@grpc/proto-loader';
+import * as GRPC from '@grpc/grpc-js';
+import { Response } from './interfaces/response.interface'
 
-describe('ModelController', () => {
+describe('test grpc on model controller', () => {
+  let server;
+  let app: INestApplication;
+  let client: any;
   let controller: ModelController;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
+    const module = await Test.createTestingModule({
       controllers: [ModelController],
-      providers: [ModelService]
+      providers: [ModelService],
+      imports: [
+        ClientsModule.register([
+          {
+            name: 'MODEL_PACKAGE',
+            transport: Transport.GRPC,
+            options: {
+              package: 'ModelGenerator',
+              protoPath: join(__dirname, '../../../generativeModelFiles/modelGenerator.proto'),
+              url: "127.0.0.1:50051"
+              
+            },
+          },
+        ]),
+      ],
     }).overrideProvider(ModelService).useValue(MockModelService).compile();
+
+    app = module.createNestApplication();
+    server = app.getHttpAdapter().getInstance();
+
+    app.connectMicroservice({
+      transport: Transport.GRPC,
+      options: {
+        package: ['model'],
+        protoPath: [
+          join(__dirname, './model.proto'),
+        ],
+      },
+    });
+    // Start gRPC microservice
+    await app.startAllMicroservices();
+    await app.init();
+    // Load proto-buffers for test gRPC dispatch
+    const proto = ProtoLoader.loadSync(
+      join(__dirname, '../model/model.proto'),
+    ) as any;
+    // Create Raw gRPC client object
+    const protoGRPC = GRPC.loadPackageDefinition(proto) as any;
+    // Create client connected to started services at standard 5000 port
+    client = new protoGRPC.model.ModelController(
+      'localhost:5000',
+      GRPC.credentials.createInsecure(),
+    );
 
     controller = module.get<ModelController>(ModelController);
   });
 
-  it('should be defined', () => {
+  it('should be defined', () => {    
+    expect(app).toBeDefined();
+  })
+
+  it('should be defined', () => {    
     expect(controller).toBeDefined();
+  })
+
+  it('GRPC Sending and receiving Stream from RX handler', async () => {
+    const dto = {data:[1,2,3]}
+    const callHandler = client.handleCoords(dto);
+
+    var sum = 0;
+    for (let i = 0; i < dto.data.length; i++) {
+      sum += dto.data[i]
+    }
+
+    callHandler.on('data', (msg: Response) => {
+      expect(msg.data).toEqual(sum);
+      callHandler.cancel();
+    });
+
+    callHandler.on('error', (err: any) => {
+      if (String(err).toLowerCase().indexOf('cancelled') === -1) {
+        fail('error : ' + err);
+      }
+    });
+
+    return new Promise((resolve,reject) => {
+      callHandler.write(dto);
+      setTimeout(() => resolve(callHandler), 1000);
+    });
+  });
+
+  afterEach(async () => {
+    await app.close();
   });
 
 });
