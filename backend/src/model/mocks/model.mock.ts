c7b22bd13625d185f84c99ab7f672de445e82c6f
ecoryndonbakeruni <u19200065@tuks.co.za>
2021-08-18 17:24:22 +0200

    updated model mock

--- backend/src/model/mocks/model.mock.ts
+++ backend/src/model/mocks/model.mock.ts
@@ -1,3 +1,4 @@
+import { join } from "path";
 import { ReplaySubject } from "rxjs";
 import { RequestProxy } from "../grpc.interface";
 
@@ -10,6 +11,22 @@ export const MockModelService= {
             sum += dto.data[i]
         }
         return sum;
+    }),
+
+    runPython: jest.fn((request) => {
+        var myPythonScriptPath = join(__dirname, '../../../../generativeModelFiles/modelGenerator.py');
+        var myPythonModelPath = join(__dirname, '../../../../generativeModelFiles/defaultModels/Epochs-50.pt');
+
+        const spawnSync = require("child_process").spawnSync;
+
+        var commaSplitList = request.data.toString().split(',');
+        var coord1 = parseFloat(commaSplitList[0]);
+        var coord2 = parseFloat(commaSplitList[1]);
+        var coord3 = parseFloat(commaSplitList[2]);
+
+        var process = spawnSync('python',["-W ignore",myPythonScriptPath,"--coordinates",coord1,coord2,coord3,"--model",myPythonModelPath]);
+        //console.log(process.stderr.toString())
+        return process.stdout.toString()
     }),
 
     proxy: jest.fn((request) => {
