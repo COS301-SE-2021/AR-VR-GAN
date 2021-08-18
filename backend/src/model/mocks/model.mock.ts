6db7dce2d6dd62143ee40dcd4a7712d2480a3ad2
ecoryndonbakeruni <u19200065@tuks.co.za>
2021-08-18 17:54:46 +0200

    model mock updated

--- backend/src/model/mocks/model.mock.ts
+++ backend/src/model/mocks/model.mock.ts
@@ -1,6 +1,4 @@
 import { join } from "path";
-import { ReplaySubject } from "rxjs";
-import { RequestProxy } from "../grpc.interface";
 
 export const MockModelService= {
 
@@ -14,26 +12,23 @@ export const MockModelService= {
     }),
 
     runPython: jest.fn((request) => {
-        var myPythonScriptPath = join(__dirname, '../../../../generativeModelFiles/modelGenerator.py');
-        var myPythonModelPath = join(__dirname, '../../../../generativeModelFiles/defaultModels/Epochs-50.pt');
+        var myPythonScriptPath = join(__dirname, './py-script.py');
 
         const spawnSync = require("child_process").spawnSync;
 
         var commaSplitList = request.data.toString().split(',');
-        var coord1 = parseFloat(commaSplitList[0]);
-        var coord2 = parseFloat(commaSplitList[1]);
-        var coord3 = parseFloat(commaSplitList[2]);
 
-        var process = spawnSync('python',["-W ignore",myPythonScriptPath,"--coordinates",coord1,coord2,coord3,"--model",myPythonModelPath]);
-        //console.log(process.stderr.toString())
+        var process = spawnSync('python',[myPythonScriptPath,commaSplitList]);
+
         return process.stdout.toString()
     }),
 
-    proxy: jest.fn((request) => {
-        const subject = new ReplaySubject<RequestProxy>();
-        subject.next({ vector: request.data });
-        subject.complete();
-        const stream = MockModelService.grpcService.generateImage(subject.asObservable());
-        return stream.toPromise();
+    proxy: jest.fn(async (request) => {
+        let sum = 0;
+
+        for (let i = 0; i < request.data.length; i++) {
+            sum += request.data[i]
+        }
+        return sum;
     })
   }
