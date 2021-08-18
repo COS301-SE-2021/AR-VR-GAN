4035ca4de0cbe1389caa59cf9b6b16581b293225
ecoryndonbakeruni <u19200065@tuks.co.za>
2021-08-18 18:27:19 +0200

    fixed dto

--- backend/src/user/dto/get-user-by-username.dto.ts
+++ backend/src/user/dto/get-user-by-username.dto.ts
@@ -1,23 +1,13 @@
-import { User } from "../interfaces/user.interface";
-
-export class GetUserByUsernameResponse {
-    readonly success: boolean;
-    readonly message: string;
-    readonly user: User;
-
-    constructor(success: boolean, message: string, user: User) {
-        this.success = success;
-        this.message = message;
-        this.user = user;
-    }
-}
 
 export class GetUserByUsernameDto {
     readonly jwtToken: string;
     readonly username: string;
+    
 
-    constructor(jwtToken: string, username: string) {
+    constructor(jwtToken:string,username: string) {
         this.jwtToken = jwtToken;
         this.username = username;
+        
     }
 }
+
