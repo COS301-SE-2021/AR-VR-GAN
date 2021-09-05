5b178a4cb8b0cfabff4998d9dfabe020a7dc3471
kabelo-tuks <u19247070@tuks.co.za>
2021-09-05 16:22:33 +0200

    Completed CNN decoder

--- generativeModelFiles/ConvolutionalAutoencoder.py
+++ generativeModelFiles/ConvolutionalAutoencoder.py
@@ -4,13 +4,21 @@ import torch.nn as nn
 import torch.optim as optim
 from torchvision import datasets, transforms
 
+class View(nn.Module):
+    def __init__(self, size):
+        super(View, self).__init__()
+        self.size = size
+
+    def forward(self, tensor):
+        return tensor.view(self.size)
+
+
 class ConvolutionalAutoencoder(nn.Module):
     def __init__(self, latent_vector: int = 3):
         super().__init__()        
         # input is Nx1x28x28
         # N, 1, 28, 28
-        last_filter: int = -(latent_vector-1-2-10)
-
+        self.z = latent_vector
         self.encoder = nn.Sequential(
             # Note we increase the channels but reduce the size of the image
             nn.Conv2d(3, 16, 11, stride=1, padding=1), # -> (N, 16 , (14, 14) <- image size);<-output, reduces image size by half; 1<- input size, 16 <- output channels, 3 <- kernal size, 
@@ -19,17 +27,24 @@ class ConvolutionalAutoencoder(nn.Module):
             nn.ReLU(),
             nn.Conv2d(32, 64, 9, stride=1, padding=1), # -> N, 64, 1, 1
             nn.ReLU(),
-            nn.Conv2d(64, 128, last_filter, stride=1, padding=1)
+            nn.Conv2d(64, 128, 12, stride=1, padding=1),
+            View((-1, 256*1*1)), 
+            nn.Linear(128, self.z)
         )
         
-        # N , 64, 1, 1 <- input
+        # N , latent_vector, <- input
         self.decoder = nn.Sequential(
-            nn.ConvTranspose2d(64, 32, 7), # -> N, 32, 7, 7
+            nn.Linear(self.z, 128),
+            View((-1, 256, 1, 1)),
             nn.ReLU(),
-            nn.ConvTranspose2d(32, 16, 3, stride=2, padding=1, output_padding=1), # N, 16, 14, 14 (N,16,13,13 without output_padding)
+            nn.ConvTranspose2d(128, 64, 12, stride=1, padding=1), # -> N, 32, 7, 7
             nn.ReLU(),
-            nn.ConvTranspose2d(16, 1, 3, stride=2, padding=1, output_padding=1), # N, 1, 28, 28  (N,1,27,27)
-            nn.Sigmoid()
+            nn.ConvTranspose2d(64, 32, 9, stride=1, padding=1), # N, 16, 14, 14 (N,16,13,13 without output_padding)
+            nn.ReLU(),
+            nn.ConvTranspose2d(32, 16, 7, stride=1, padding=1), # N, 1, 28, 28  (N,1,27,27)
+            nn.ReLU(),
+            nn.ConvTranspose2d(16, 3, 11, stride=1, padding=1), # N, 1, 28, 28  (N,1,27,27)
+            # nn.Sigmoid()
         )
 
     def forward(self, x):
