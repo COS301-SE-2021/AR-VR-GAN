17f27db77c63e82e146bb300794d1f774329b1f1
ecoryndonbakeruni <u19200065@tuks.co.za>
2021-09-15 09:30:21 +0200

    added interface and function to grpc

--- backend/src/model/grpc.interface.ts
+++ backend/src/model/grpc.interface.ts
@@ -2,8 +2,13 @@ import { Observable } from 'rxjs';
 
 export interface ModelGeneration {
   generateImage(Request: Observable<RequestProxy>): Observable<any>;
+  LoadModel(Request: RequestModel): any;
 }
 
 export interface RequestProxy {
   vector: number[];
+}
+
+export interface RequestModel {
+  modelName: string;
 }
