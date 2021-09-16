00d8c8b8bb00a8e1ca78e86947a2c5b11bcbee3c
Jason Wille <jman89412@gmail.com>
2021-09-16 19:57:58 +0200

    Update train-model.dto.ts

--- backend/src/model/dto/train-model.dto.ts
+++ backend/src/model/dto/train-model.dto.ts
@@ -7,13 +7,15 @@ export class trainModelDto {
     readonly datasetName: string;
     readonly beta: number;
     readonly modelType: string;
+    readonly jwtToken: string;
 
-    constructor(modelName: string,trainingEpochs: number,latentSize: number,datasetName: string,beta: number,modelType: string) {
-        this.modelName = modelName;
+    constructor(modelName: string,trainingEpochs: number,latentSize: number,datasetName: string,beta: number,modelType: string,jwtToken: string) {
+        this.modelName= modelName;
         this.trainingEpochs= trainingEpochs;
         this.latentSize= latentSize;
         this.datasetName= datasetName;
         this.beta= beta;
         this.modelType= modelType;
+        this.jwtToken= jwtToken;
     }
 }
