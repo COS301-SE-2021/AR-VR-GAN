21196114cb0e6c1b59725e6894d611726103cc6d
Jason Wille <jman89412@gmail.com>
2021-09-16 15:03:09 +0200

    New component train-model.

---
+++ frontend/src/app/components/train-model/train-model.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-train-model',
+  templateUrl: './train-model.component.html',
+  styleUrls: ['./train-model.component.css']
+})
+export class TrainModelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
