34d5925c607a56c5fbb7b1ba3d5cd8362e088e54
Jason Wille <jman89412@gmail.com>
2021-09-16 10:32:19 +0200

    Current value

--- frontend/src/app/components/customize/customize.component.ts
+++ frontend/src/app/components/customize/customize.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { HOST_URL } from 'src/config/consts';
 
 @Component({
   selector: 'app-customize',
@@ -8,7 +10,9 @@ import { Component, OnInit } from '@angular/core';
 export class CustomizeComponent implements OnInit {
   selected: string;
 
-  constructor() { 
+  constructor(
+    private http: HttpClient
+  ) { 
     this.selected = "option1";
   }
 
@@ -16,7 +20,22 @@ export class CustomizeComponent implements OnInit {
   }
 
   fetchCurrentValues(): void {
+    console.log('Fetching');
 
+    this.http.post<any>(HOST_URL + '/model/listModels', {
+      'default': false,
+      'saved': true
+    }).subscribe(resp => {
+      console.log(resp);
+    });
+  }
+
+  bin2string(array: any){
+    var result = "";
+    for(var i = 0; i < array.length; ++i){
+      result+= (String.fromCharCode(array[i]));
+    }
+    return result;
   }
 
   fetchAvailableDatasets(): void {
