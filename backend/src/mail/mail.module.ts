6ddd039530408f4a2a09f86c29fefeb6830c5888
ecoryndonbakeruni <u19200065@tuks.co.za>
2021-09-16 17:11:25 +0200

    mail module added

---
+++ backend/src/mail/mail.module.ts
@@ -0,0 +1,35 @@
+import { MailerModule } from '@nestjs-modules/mailer';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+import { Module } from '@nestjs/common';
+import { MailService } from './mail.service';
+import { join } from 'path';
+
+@Module({
+  imports: [
+    MailerModule.forRoot({
+      // transport: 'smtps://user@example.com:topsecret@smtp.example.com',
+      // or
+      transport: {
+        host: 'smtp.gmail.com',
+        //secure: false,
+        auth: {
+          user: 'javacinsomniacs@gmail.com',
+          pass: 'MEJZK2021',
+        },
+      },
+      defaults: {
+        from: '"No Reply" <javacinsomniacs@gmail.com>',
+      },
+      template: {
+        dir: join(__dirname, 'templates'),
+        adapter: new HandlebarsAdapter(),
+        options: {
+          strict: true,
+        },
+      },
+    }),
+  ],
+  providers: [MailService],
+  exports: [MailService], 
+})
+export class MailModule {}
