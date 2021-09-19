577ef5818db75707d926c006db804d2cb0fa1081
ZachariahIdiculla <u17067210@tuks.co.za>
2021-09-19 19:16:36 +0200

    Update mail.service.spec.ts

--- backend/src/mail/mail.service.spec.ts
+++ backend/src/mail/mail.service.spec.ts
@@ -5,9 +5,12 @@ import config from '../config/keys';
 import { join } from 'path';
 import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
 import { MailModule } from './mail.module';
+import { sendEmailDto } from './dto/send-email.dto';
+import MockMailClass, { MockMailService } from './mocks/mail.mock';
 
 describe('MailService', () => {
   let service: MailService;
+  let mockService: MockMailClass
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -15,12 +18,20 @@ describe('MailService', () => {
       imports:[
         MailModule
       ]
-    }).compile();
+    }).overrideProvider(MailService).useValue(MockMailService).compile();
 
     service = module.get<MailService>(MailService);
+     
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('send mail definied', async () => {
+    const emailDto = new sendEmailDto("test","test","test");
+    expect(await service.sendConfirmationEmail(emailDto)).toBeDefined();
+  });
+
+
 });
