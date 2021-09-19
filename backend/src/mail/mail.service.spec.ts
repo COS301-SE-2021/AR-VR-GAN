95e0ff8c652750f8433cb6cee6102858e1dd3cd8
ecoryndonbakeruni <u19200065@tuks.co.za>
2021-09-19 13:14:42 +0200

    mail service testing

--- backend/src/mail/mail.service.spec.ts
+++ backend/src/mail/mail.service.spec.ts
@@ -1,5 +1,10 @@
+import { MailerModule } from '@nestjs-modules/mailer';
 import { Test, TestingModule } from '@nestjs/testing';
 import { MailService } from './mail.service';
+import config from '../config/keys';
+import { join } from 'path';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+import { MailModule } from './mail.module';
 
 describe('MailService', () => {
   let service: MailService;
@@ -7,6 +12,9 @@ describe('MailService', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [MailService],
+      imports:[
+        MailModule
+      ]
     }).compile();
 
     service = module.get<MailService>(MailService);
