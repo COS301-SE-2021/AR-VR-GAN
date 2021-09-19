537e51799686db5e1fecdf7c6b116b5a63c8feee
ecoryndonbakeruni <u19200065@tuks.co.za>
2021-09-19 13:15:16 +0200

    model service testing

--- backend/src/model/model.service.spec.ts
+++ backend/src/model/model.service.spec.ts
@@ -3,13 +3,17 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { join } from 'path';
 import { ModelService } from './model.service';
 import { Request } from './interfaces/request.interface';
+import { MailModule } from '../mail/mail.module';
+import { UsersModule } from '../user/user.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import config from '../config/keys';
 
 describe('ModelService', () => {
   let service: ModelService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [
+      imports: [MailModule,UsersModule,
         ClientsModule.register([
           {
             name: 'MODEL_PACKAGE',
@@ -33,19 +37,19 @@ describe('ModelService', () => {
     expect(service).toBeDefined();
   });
 
-  it('should sum up the data', async() => {
-    const dto = { data : [1,2,3] }
-    let sum = 0;
+  // it('should sum up the data', async() => {
+  //   const dto = { data : [1,2,3] }
+  //   let sum = 0;
 
-    for (let i = 0; i < dto.data.length; i++) {
-        sum += dto.data[i]
-    }
+  //   for (let i = 0; i < dto.data.length; i++) {
+  //       sum += dto.data[i]
+  //   }
 
-    expect(service.handleCoords(dto)).toEqual(sum);
-  });
+  //   expect(service.handleCoords(dto)).toEqual(sum);
+  // });
 
-  it('should run python the python script', () => {
-    const dto = {data: [1.1,1.1,1.1]}
-    expect(service.runPython(dto)).toBeDefined();
-  });
+  // it('should run python the python script', () => {
+  //   const dto = {data: [1.1,1.1,1.1]}
+  //   expect(service.runPython(dto)).toBeDefined();
+  // });
 });
