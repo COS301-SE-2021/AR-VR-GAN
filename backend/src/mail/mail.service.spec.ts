99e71f1d2ac78f5598d6db759b3b61a204747c4a
ecoryndonbakeruni <u19200065@tuks.co.za>
2021-09-19 20:11:17 +0200

    cleaned mail testing

--- backend/src/mail/mail.service.spec.ts
+++ backend/src/mail/mail.service.spec.ts
@@ -1,9 +1,5 @@
-import { MailerModule } from '@nestjs-modules/mailer';
 import { Test, TestingModule } from '@nestjs/testing';
 import { MailService } from './mail.service';
-import config from '../config/keys';
-import { join } from 'path';
-import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
 import { MailModule } from './mail.module';
 
 describe('MailService', () => {
