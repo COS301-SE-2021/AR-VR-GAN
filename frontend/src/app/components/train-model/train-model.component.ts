e5c5a483017e0865465277c500eef985af4e5d45
Jason Wille <jman89412@gmail.com>
2021-09-20 15:50:04 +0200

    Update train-model.component.ts

--- frontend/src/app/components/train-model/train-model.component.ts
+++ frontend/src/app/components/train-model/train-model.component.ts
@@ -67,7 +67,7 @@ export class TrainModelComponent implements OnInit {
       return;
     }
 
-    if (!((datasetName == "mnist") || (datasetName == "fashion") || (datasetName == "cifar10") || (datasetName == "celeba"))) {
+    if (!((datasetName == "mnist") || (datasetName == "fashion"))) {
       this.snackBar.open("Please choose a valid dataset", "Close");
       return;
     }
